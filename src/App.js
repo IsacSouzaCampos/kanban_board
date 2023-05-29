7ababbbe6ea9d754355785c6f150ae1f44317bc2
Isac Campos <isacsouzacampos@hotmail.com>
2023-05-29 17:05:56 -0300

    Aplicação da biblioteca DragDropContext.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+
+const defaultColumns = [
+  {
+    name: "A Fazer",
+    id: "1",
+    items: [{id: '11', title: 'Exemplo 1', content: 'Esse é o exemplo 1.'},
+            {id: '12', title: 'Exemplo 2', content: 'Esse outro é o exemplo 2.'}],
+  },
+  {
+    name: "Fazendo",
+    id: "2",
+    items: [{id: '21', title: 'Exemplo 3', content: 'Aqui temos o exemplo 3.'},],
+  },
+  {
+    name: "Feito",
+    id: "3",
+    items: [],
+  }
+];
 
 function App() {
+  const [columns] = useState(defaultColumns);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext>
+        {columns.map((column, index) => (
+          <div class='column'>
+            <h1 class='name'>{column.name}</h1>
+            <Droppable droppableId={column.id}>
+              {(provided) => (
+                <div ref={provided.innerRef} class='column-content'>
+                  {column.items.map((item, index) => (
+                    <Draggable draggableId={item.id} index={index}>
+                      {(provided) => (
+                        <div
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          ref={provided.innerRef}
+                          class='item'
+                        >
+                          <div class='title'>{item.title}</div>
+                          <div class='item-content'>{item.content}</div>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        ))}
+      </DragDropContext>
     </div>
   );
 }
